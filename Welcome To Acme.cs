675741eb608332eea03913471e84d2426e8a2a84
Charles Montemayor <charlesdarbyn@outlook.com>
2019-03-19 10:25:14 -0600

    Welcome to Acme.cs

---
+++ Welcome To Acme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome To Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
