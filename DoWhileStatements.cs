d7b27f917cbffb207d078309bf9cc7a2bfaeee0d
Charles Montemayor <charlesdarbyn@outlook.com>
2019-03-28 14:42:25 -0600

    added 'DoWhileStatement.cs' & 'WhileStatement.cs' file

---
+++ DoWhileStatements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DoWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a letter: ");
+            string input = Console.ReadLine();
+            string letter = input.ToLower();
+            bool isLetter = letter == "m";
+
+            do
+            {
+                switch (letter)
+                {
+                    case "m":
+                        Console.WriteLine("You have guessed the letter M. You are correct!");
+                        isLetter = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect. Try again.");
+                        Console.WriteLine("Guess a letter: ");
+                        letter = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isLetter);
+            Console.Read();
+        }
+    }
+}
---
+++ WhileStatement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a letter: ");
+            string letter = Console.ReadLine();
+            bool isLetter = letter == "m";
+
+            while (!isLetter)
+            {
+                switch (letter)
+                {
+                    case "m":
+                        Console.WriteLine("You have guessed the letter M. You are correct!");
+                        isLetter = true;
+                        break;
+                    default:
+                        Console.WriteLine("That is incorrect. Try again.");
+                        Console.WriteLine("Guess a letter: ");
+                        letter = Console.ReadLine();
+                        break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
