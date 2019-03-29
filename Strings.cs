041d4e8e1cfa20470b3d356f4dd618612b5869f1
Charles Montemayor <charlesdarbyn@outlook.com>
2019-03-29 06:44:25 -0600

    added 'Strings.cs' file

---
+++ Strings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Charles";
+            string startGreet = "Hello!";
+            string endGreet = "Have a good day.";
+
+            Console.WriteLine(startGreet + " My name is " + name + ". " + endGreet);
+
+            Console.WriteLine(endGreet.ToUpper());
+
+            StringBuilder sb = new StringBuilder("Hello World! ");
+            sb.Append("My name is " + name + ". ");
+            sb.AppendLine("I am studying C#. ");
+            sb.AppendLine("I love C#. ");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
