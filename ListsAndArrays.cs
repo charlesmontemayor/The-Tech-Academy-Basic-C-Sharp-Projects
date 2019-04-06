4ce371530f207211284a207ad5ae5fefac9b4b5c
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-06 16:37:59 -0600

    added ListsAndArrays.cs

---
+++ ListsAndArrays.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+            //List<string> words = new List<string>() { "I", "love", "studying", "C#" };
+            //Console.WriteLine("Enter new words.");
+            //words.Add(Console.ReadLine());
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            //Console.ReadLine();
+
+            //#2
+            //for (int i = 0; ; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //#3
+            //for (int i = 0; ; i++)
+            //{
+            //    if (i == 100000)
+            //        break;
+            //    Console.WriteLine(i);
+            //}
+
+            //#4
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //#5
+            //for (int i = 0; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //#6
+            //List<string> words = new List<string> { "red", "blue", "green", "yellow", "orange", "pink", "brown", "violet" };
+            //Console.WriteLine("Enter a color name to search in the list.");
+            //string input = Console.ReadLine();
+            //string color = input.ToLower();
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine(word + " = " + words.LastIndexOf(word));
+            //}
+
+            //#7 + #8
+            //List<string> words = new List<string> { "red", "blue", "green", "yellow", "orange", "pink", "brown", "violet" };
+            //Console.WriteLine("Enter a color name to search in the list.");
+            //string input = Console.ReadLine();
+            //string color = input.ToLower();
+            //if (words.Contains(color))
+            //{
+            //    Console.WriteLine("The index of the color " + color + " in the list is " + words.LastIndexOf(color));
+            //}
+            //else if (!words.Contains(color))
+            //{
+            //    Console.WriteLine("That color is not in the list.");
+            //}
+
+            //#9 + #10
+            //List<string> words = new List<string> { "red", "blue", "green", "yellow", "orange", "pink", "brown", "violet", "green", "blue" };
+            //Console.WriteLine("Enter a color name to search in the list.");
+            //string input = Console.ReadLine();
+            //string color = input.ToLower();
+            //foreach (string word in words)
+            //{
+            //    Console.WriteLine(word + " = " + words.LastIndexOf(word));
+            //}
+            //if (words.Contains(color))
+            //{
+            //    Console.WriteLine("The index of the color " + color + " in the list is " + words.LastIndexOf(color));
+            //}
+            //else if (!words.Contains(color))
+            //{
+            //    Console.WriteLine("That color is not in the list.");
+            //}
+
+
+        }
+    }
+}
