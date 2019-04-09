42d4e0be8539c0340f8b69c803837456d29a01cb
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-09 17:29:04 -0600

    added CurrencyConverter

---
+++ CurrencyConverter/CurrencyConverter/CurrentExchangeRates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class CurrentExchangeRates
+    {
+        public static double toBitcoin(double amount)
+        {
+            double result = amount * 0.0019;
+            return result;
+        }
+        public static double toEuro(double amount)
+        {
+            double result = amount * 0.77;
+            return result;
+        }
+        public static double toPound(double amount)
+        {
+            double result = amount * 0.89;
+            return result;
+        }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will convert dollar to euro, pound and bitcoin. \nEnter the desired amount.");
+            string input = Console.ReadLine();
+            double amount = Convert.ToDouble(input);
+
+            double euro = CurrentExchangeRates.toEuro(amount);
+            Console.WriteLine("€ " + euro);
+
+            double pound = CurrentExchangeRates.toPound(amount);
+            Console.WriteLine("£ " + pound);
+
+            double bitcoin = CurrentExchangeRates.toBitcoin(amount);
+            Console.WriteLine("Bitcoin " + bitcoin);
+        }
+
+    }
+}
