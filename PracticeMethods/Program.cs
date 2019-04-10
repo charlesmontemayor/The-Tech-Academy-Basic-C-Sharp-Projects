19882f32fa6f5f1ffed3932dbdc68b7cbe754356
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-10 08:18:30 -0600

    added PracticeMethods

---
+++ PracticeMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PracticeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConversion temp = new TemperatureConversion();
+            Console.WriteLine(temp.convert(12) + " F");
+            Console.WriteLine(temp.convert(98.6m) + " C");
+
+            Console.WriteLine("Enter a number.");
+            string input = Console.ReadLine();
+            Console.WriteLine(temp.convert(input));
+        }
+    }
+}
---
+++ PracticeMethods/TemperatureConversion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PracticeMethods
+{
+    public class TemperatureConversion
+    {
+        public int convert(int number)
+        {
+            int result = (number * 9 / 5) + 32;
+            return result;
+        }
+
+        public decimal convert(decimal number)
+        {
+            decimal result = (number - 32) * 5/9;
+            return result;
+        }
+
+        public int convert(string number)
+        {
+            int result = Int32.Parse(number);
+            return result;
+        }
+    }
+}
