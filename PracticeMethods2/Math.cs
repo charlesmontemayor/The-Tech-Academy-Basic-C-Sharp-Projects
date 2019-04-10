0b39cf1c241c72414e26bc5142f1e14258772f78
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-10 10:20:18 -0600

    added PracticeMethods2

---
+++ PracticeMethods2/Math.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PracticeMethods2
+{
+    public class Math
+    {
+        public int Add(int num1, int num2 = 0)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ PracticeMethods2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PracticeMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number (This can be optional).");
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("{0} + {1} = {2}", num1, input, math.Add(num1));
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(input);
+                Console.WriteLine("{0} + {1} = {2}", num1, num2, math.Add(num1, num2));
+            }
+        }
+    }
+}
