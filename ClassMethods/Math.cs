a46958cf9be03aa5cf038d1207c6290041443a25
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-11 17:43:00 -0600

    added ClassMethods

---
+++ ClassMethods/Math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ClassMethods
+{
+    public class Math
+    {
+        public void Divide(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine("{0} ÷ 2 = {1}", num1, result);
+        }
+
+        public int Remainder(int num1, out int num2)
+        {
+            num2 = 0;
+            double result = num1 / 2;
+            if (num1 % 2 == 1)
+            {
+                int remainder = num1 % 2;
+                Console.WriteLine("{0} + 2 = {1} with as remainder of {2}", num1, Convert.ToInt32(result), remainder);
+                num2 = remainder;
+            }
+            return num2;
+        }
+
+        public void Divide(double num1)
+        {
+            double result = num1 / 2;
+            Console.WriteLine("{0} ÷ 2 = {1}", num1, result);
+        }
+
+        public static void Division(double num1)
+        {
+            double result = num1 / 2;
+            Console.WriteLine("{0} divided by 2 is equal to {1}.", num1, result);
+        }
+    }
+}
---
+++ ClassMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number.");
+            string input = Console.ReadLine();
+            double num1 = Convert.ToDouble(input);
+
+            Math math = new Math();
+
+            math.Divide(num1);
+
+            math.Remainder(Convert.ToInt32(num1), out int num2);
+
+            Math.Division(num1);
+        }
+    }
+}
