33d5a3e38083fe5a7e425e2876afb65f06f1cdec
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-17 14:29:26 -0600

    added Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it today?");
+            string input = Console.ReadLine();
+            try
+            {
+                foreach(string day in Enum.GetNames(typeof(DaysOfTheWeek)))
+                {
+                    if (day == input)
+                    {
+                        Console.WriteLine("Today is " + day);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
