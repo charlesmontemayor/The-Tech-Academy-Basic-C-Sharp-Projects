e7d908f514f1c6da8ca37985531ce389803a5382
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-18 10:04:20 -0600

    added struct

---
+++ Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 100.100m;
+
+            Console.WriteLine(number.Amount);
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
