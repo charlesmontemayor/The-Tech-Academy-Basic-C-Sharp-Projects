b69caac993741752a2b2223ca9d89505ab9e7be3
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-24 16:00:47 -0600

    added FileIO

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your phone number?");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"/Users/charlesm/Projects/FileIO/Log/log.txt", number);
+            Console.WriteLine(File.ReadAllText(@"/Users/charlesm/Projects/FileIO/Log/log.txt"));
+
+        }
+    }
+}
