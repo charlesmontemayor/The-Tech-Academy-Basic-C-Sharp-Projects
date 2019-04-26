abc8c4b43e710e8fb44f189164b5ef50a42db451
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-26 14:14:36 -0600

    Added SampleDateTime

---
+++ SampleDateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleDatetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Right now, it is {0}.", DateTime.Now);
+            Console.WriteLine("Enter a number of hours.");
+            int inputHours = Convert.ToInt32(Console.ReadLine());
+            DateTime currentTime = DateTime.Now;
+            DateTime hoursLater = currentTime.AddHours(inputHours);
+            Console.WriteLine("In {0} hours, it will be at {1}.", inputHours, hoursLater);
+        }
+    }
+}
