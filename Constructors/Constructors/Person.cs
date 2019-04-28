7ed32ff18d170e27ac49a089987892510d54c945
Charles Montemayor <charlesdarbyn@outlook.com>
2019-04-27 21:03:23 -0600

    added Constructors

---
+++ Constructors/Constructors/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Constructors
+{
+    public class Person
+    {
+        public Person(string name) : this(name, 1)
+        {
+        }
+        public Person(string name, int number)
+        {
+            Name = name;
+            PlaceNumber = number;
+        }
+        public string Name { get; set; }
+        public int PlaceNumber { get; set; }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string greetings = "Hello! ";
+
+            var person = new Person("Charles");
+
+            Console.WriteLine(greetings + person.Name);
+        }
+    }
+}
